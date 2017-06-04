9d142009b953f8a375ba69f324ffaa00846bed5e
Kirill Krasnozhenov <krasnozhenovk@gmail.com>
2017-06-04 21:51:58 +0300

    creating server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ server.route({
     method: 'GET',
     path: '/',
     handler: function (request, reply) {
-        reply('Hello, world!');
+        reply('hhh!');
     }
 });
 
@@ -21,10 +21,6 @@ server.route({
     }
 });
 
-server.start((err) => {
-
-    if (err) {
-        throw err;
-    }
-    console.log(`Server running at: ${server.info.uri}`);
+server.start(function () {
+    console.log('Server running at:', server.info.uri);
 });
