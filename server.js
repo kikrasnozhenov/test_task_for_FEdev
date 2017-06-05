fe5fba86ec7afebee22c128bb9bdd94dcd980df2
Kirill Krasnozhenov <krasnozhenovk@gmail.com>
2017-06-05 12:28:12 +0300

    uploaded inert module and created server.js with routes

--- server.js
+++ server.js
@@ -5,19 +5,27 @@ const Hapi = require('hapi');
 const server = new Hapi.Server();
 server.connection({ port: 8080, host: 'localhost' });
 
-server.route({
-    method: 'GET',
-    path: '/',
-    handler: function (request, reply) {
-        reply('hhh!');
+server.register(require('inert'), function (err) {
+    if (err) {
+        throw err;
     }
+
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: function (request, reply) {
+            reply.file('./public/index.html');
+        }
+    });
 });
 
 server.route({
     method: 'GET',
-    path: '/{name}',
-    handler: function (request, reply) {
-        reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
+    path: '/{param*}',
+    handler: {
+        directory: {
+            path: './public/'
+        }
     }
 });
 
