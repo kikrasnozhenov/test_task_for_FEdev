b9618ed9861426d256614ce846ac8fbf89856ac0
Kirill Krasnozhenov <krasnozhenovk@gmail.com>
2017-06-06 00:55:52 +0300

    began create routes

---
+++ public/index.js
@@ -0,0 +1,14 @@
+angular.module('PhotosApp', ['ngRoute'])
+
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+    .when("/", {
+      templateUrl : "gallery.html"
+    })
+    .when("/photos", {
+      templateUrl : "photos.html"
+    })
+    .otherwise({ 
+          redirectTo: '/'
+      });
+}]);
