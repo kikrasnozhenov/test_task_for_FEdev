ba9b983cd776067611c56d01b844719167de38d9
Kirill Krasnozhenov <krasnozhenovk@gmail.com>
2017-06-08 00:59:33 +0300

    started to develop service.js and fixed ctrl and index.html

--- public/galleryCtrl.js
+++ public/galleryCtrl.js
@@ -3,3 +3,13 @@ var app = angular.module('app', ['ngRoute']);
 app.controller('photosCtrl', photosCtrl);
 
 photosCtrl.$inject = ['Photos'];
+
+var app = angular.module('app', ['ngRoute']);
+app.controller('photosCtrl', photosCtrl);
+
+photosCtrl.$inject = ['Photos'];
+function photosCtrl(Photos) {
+  var vm = this;
+  this.addPhoto = function() {
+  };
+}
